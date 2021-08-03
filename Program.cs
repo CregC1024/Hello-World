5d50cc35861ebda69a9b91ef795c4b237fed048d
CregC1024 <38873613+CregC1024@users.noreply.github.com>
2021-08-03 15:56:25 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloCregC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Creg C !");
+        }
+    }
+}
